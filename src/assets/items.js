fa9a8844ceb86c9a7a41f2e15588d044eb9c374a
vnunez85 <82461574+vnunez85@users.noreply.github.com>
2021-06-26 16:59:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/assets/items.js
@@ -0,0 +1,53 @@
+import website from '../assets/website.png'
+import instagram from '../assets/instagram.png'
+import appstore from '../assets/IMG_20201124_112759.png'
+import youtube from '../assets/images (3).jpeg'
+import dribbble from '../assets/dribbble.png'
+import github from '../assets/github.png'
+import telegram from '../assets/IMG_20201124_104554.png'
+
+const items = [{
+    "title": "Website",
+    "subtitle": "Look at my work!",
+    "image": website,
+    "link": "#"
+},
+{
+    "title": "GitHub",
+    "subtitle": "@devgossips | 🏠 of my open-source projects",
+    "image": github,
+    "link": "https://github.com/devgossips"
+},
+{
+    "title": "Instagram",
+    "subtitle": "@virtuonic100 | Hub of my awesome 😎pics",
+    "image": instagram,
+    "link": "https://instagram.com/virtuonic100"
+},
+{
+    "title": "Apps",
+    "subtitle": "Hub of my awesome🔥 Apps",
+    "image": appstore,
+    "link": "#"
+},
+{
+    "title": "YouTube",
+    "subtitle": "@Sagnik Sahoo | Official channel of mine",
+    "image": youtube,
+    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"
+},
+
+{
+    "title": "Dribbble",
+    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
+    "image": dribbble,
+    "link": "https://dribbble.com/virtuonic"
+},
+{
+    "title": "Telegram",
+    "subtitle": "@virtuonic | Chat with me instantly, Don't hesitate! ",
+    "image": telegram,
+    "link": "https://telegram.me/virtuonic"
+}]
+
+export default items
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import '../css/skeleton.css'
+import '../css/normalize.css'
+import '../css/components.css'
+
+function Header() {
+    return (
+        <div className = "Header container">
+            <div className = "ten columns">
+             Sagnik
+           
+            </div>
+           
+            
+            {/*<div className = "two columns">
+                <button>
+                    <feather className="icons sun"></feather>
+                </button>
+            </div>*/}
+        </div>
+    )
+}
+
+export default Header
